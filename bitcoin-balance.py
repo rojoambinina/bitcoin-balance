72d292c5cda563c748a1831a33373e16fd84d949
rojoambinina <89576432+rojoambinina@users.noreply.github.com>
2021-08-26 17:15:20 +0300

    Update bitcoin-balance.py

--- bitcoin-balance.py
+++ bitcoin-balance.py
@@ -1,4 +1,4 @@
-# Copyrith 2016 - written by Rojo Ambinina
+# Copyright 2016 - written by Rojo Ambinina
 
 #  This code is only for generating compressed and uncompressed bitcoins addresses with their balances, and also displays the private keys at the same time.
 #  The algorithm generates bitcoin addresses until it finds a non-zero balance and stores it in a text file "Lottery_BTC.txt"
